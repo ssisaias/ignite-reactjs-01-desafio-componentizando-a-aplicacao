dfb2bcc7ee2a2830515f356e58a09c980791367c
18316-55 <norreply@gitgud.io>
2021-09-02 17:25:22 +0000

    refactor move sidebar to component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface props {
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+  buttonclick: Function;
+}
+
+export function SideBar({selectedGenreId,genres, buttonclick}: props) {
+  
+  
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => buttonclick(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
